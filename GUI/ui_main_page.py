28e11bd9a07ba9ed5a684313d1254aa3bb1b5df6
nieckojakub <niecko.jakub@gmail.com>
2022-04-16 19:48:10 +0200

    add splash window

---
+++ GUI/ui_main_page.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_pagenyXKAw.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"My app", None))
+    # retranslateUi
+
---
+++ GUI/ui_splash.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'splashsjCZjL.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_SplashScreen(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(680, 400)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(10, 10, 10, 10)
+        self.dropShadowFrame = QFrame(self.centralwidget)
+        self.dropShadowFrame.setObjectName(u"dropShadowFrame")
+        self.dropShadowFrame.setStyleSheet(u"QFrame{\n"
+"	background-color: #2b3038;\n"
+"	color: rgb(220,220,220);\n"
+"	border-radius: 15px;\n"
+"}")
+        self.dropShadowFrame.setFrameShape(QFrame.StyledPanel)
+        self.dropShadowFrame.setFrameShadow(QFrame.Raised)
+        self.label_title = QLabel(self.dropShadowFrame)
+        self.label_title.setObjectName(u"label_title")
+        self.label_title.setGeometry(QRect(0, 50, 651, 141))
+        font = QFont()
+        font.setFamily(u"Segoe UI")
+        font.setPointSize(40)
+        self.label_title.setFont(font)
+        self.label_title.setStyleSheet(u"color: rgb(212, 192, 169);")
+        self.label_title.setAlignment(Qt.AlignCenter)
+        self.label_description = QLabel(self.dropShadowFrame)
+        self.label_description.setObjectName(u"label_description")
+        self.label_description.setGeometry(QRect(0, 150, 651, 41))
+        font1 = QFont()
+        font1.setFamily(u"Segoe UI")
+        font1.setPointSize(14)
+        self.label_description.setFont(font1)
+        self.label_description.setStyleSheet(u"color: #837c73")
+        self.label_description.setAlignment(Qt.AlignCenter)
+        self.progressBar = QProgressBar(self.dropShadowFrame)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(60, 260, 531, 31))
+        self.progressBar.setStyleSheet(u"QProgressBar{\n"
+"	background-color: #20242a;\n"
+"	color: rgb(200,200,200);\n"
+"	border-radius: 10px;\n"
+"	border-style: none;\n"
+"    text-align: center;\n"
+"}\n"
+"QProgressBar::chunk{\n"
+"	border-radius: 10px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0.574, x2:1, y2:0.602, stop:0 rgba(212, 192, 169, 255), stop:1 rgba(157, 142, 126, 255));\n"
+"\n"
+"}\n"
+"")
+        self.progressBar.setValue(24)
+        self.label_loading = QLabel(self.dropShadowFrame)
+        self.label_loading.setObjectName(u"label_loading")
+        self.label_loading.setGeometry(QRect(0, 290, 651, 31))
+        font2 = QFont()
+        font2.setFamily(u"Segoe UI")
+        font2.setPointSize(11)
+        self.label_loading.setFont(font2)
+        self.label_loading.setStyleSheet(u"color: #837c73")
+        self.label_loading.setAlignment(Qt.AlignCenter)
+        self.label_credits = QLabel(self.dropShadowFrame)
+        self.label_credits.setObjectName(u"label_credits")
+        self.label_credits.setGeometry(QRect(0, 340, 651, 31))
+        font3 = QFont()
+        font3.setFamily(u"Segoe UI")
+        font3.setPointSize(7)
+        self.label_credits.setFont(font3)
+        self.label_credits.setStyleSheet(u"color: #837c73")
+        self.label_credits.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.verticalLayout.addWidget(self.dropShadowFrame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, SplashScreen):
+        SplashScreen.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_title.setText(QCoreApplication.translate("MainWindow", u"<strong>KASTA</strong> VA", None))
+        self.label_description.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-weight:600;\">Virtual Assistant</span></p></body></html>", None))
+        self.label_loading.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-weight:600;\">loading...</span></p></body></html>", None))
+        self.label_credits.setText(QCoreApplication.translate("MainWindow", u"<strong>Created</strong> Jakub Nie\u0107ko Dominik Ziomek", None))
+    # retranslateUi
+
