403e12d176bada349911c8b5deb7c2c9046a5ffd
Dziomek <dziomek@student.agh.edu.pl>
2022-05-17 17:36:14 +0200

    news, jokes, youtube, wolfram method restructuring (V2)

--- kasta/youtube/youtube_playing.py
+++ kasta/youtube/youtube_playing.py
@@ -6,7 +6,7 @@ class YoutubeService:
     @staticmethod
     def play_on_yt(text, key_word):
         movie = text.split(key_word, 2)
-        pywhatkit.playonyt(movie[1])
+        pywhatkit.playonyt(movie[1].strip())
 
 
 
